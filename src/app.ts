04bdd750b85c1283e5741afd24794f225600ad30
juliap <juliap@ciandt.com>
2021-10-26 14:14:47 -0300

    adding services, controlles and tables in prisma

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import "dotenv/config";
 import express from "express";
 import router from "./routes";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
+
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
@@ -19,4 +37,4 @@ app.get("/signin/callback", (req, res) => {
   return res.json(code);
 });
 
-app.listen(4000, () => console.log("Server running on PORT 4000"));
+export { serverHttp, io };
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { sign } from "jsonwebtoken";
+import prismaClient from "../prisma";
 
 interface IAccessTokenResponse {
   access_token: string;
@@ -7,19 +9,22 @@ interface IAccessTokenResponse {
 interface IUserResponse {
   avatar_url: string;
   login: string;
-  id: string;
+  id: number;
   name: string;
 }
 
 class AuthenticateUserService {
   async execute(code: string) {
-    const url = "https://github.com/login/oauth/acess_token";
+    const url = "https://github.com/login/oauth/access_token";
+
+    const client_id = process.env.GITHUB_CLIENT_ID;
+    const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
     const { data: accessTokenResponse } =
       await axios.post<IAccessTokenResponse>(url, null, {
         params: {
-          client_id: process.env.GITHUB_CLIENT_ID,
-          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          client_id,
+          client_secret,
           code,
         },
         headers: {
@@ -36,7 +41,41 @@ class AuthenticateUserService {
       }
     );
 
-    return response.data;
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
